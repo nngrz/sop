bc0c758fd89c4dd69a971ee73fcc2ddeb0a5750c
Ningrui Zhang <ningrui1227@gmail.com>
2025-04-09 14:55:14 +0100

    remove white space in 0

--- fibonacciRecursion.sh
+++ fibonacciRecursion.sh
@@ -2,23 +2,23 @@
 
 # This script calculates the n-th Fibonacci number - with recursion
 
-fibonacci() {
-
-    if [ "$1" -le 1 ]; then
-        echo "$1"
-    else
-        local prev1=$(fibonacci "$(( $1 - 1 ))")
-        local prev2=$(fibonacci "$(( $1 - 2 ))")
-        echo $((prev1 + prev2))
-    fi
-}
-
 # Check if user provided an argument
 if [ -z "$1" ] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
     echo "Usage: $0 <n>"
     echo "You should provide a non-negative integer."
     exit 1
 fi 
+
+fibonacci() {
+
+    if [ "$1" -le 1 ]; then
+        echo "$1"
+    else
+        local prev1=$(fibonacci "$(($1 - 1))")
+        local prev2=$(fibonacci "$(($1 - 2))")
+        echo $((prev1 + prev2))
+    fi
+}
 
 # Call the recursive function
 result=$(fibonacci "$1")
