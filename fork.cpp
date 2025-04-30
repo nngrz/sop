74e180165f2dcb9a75b62d4abc2d15c396c09924
Ningrui Zhang <ningrui1227@gmail.com>
2025-04-30 15:27:21 +0800

    add fork.cpp

---
+++ fork.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int i;
+    pid_t pid;
+
+    printf("Parent process (PID: %d)\n", getpid());
+
+    for (i = 0; i < 32; i++) {
+        pid = fork();
+
+        if (pid < 0) {
+            fprintf(stderr, "Fork failed!\n");
+            return 1;
+        } else if (pid == 0) {
+            printf("Child %d (PID: %d, Parent PID: %d)\n", i, getpid(), getppid());
+            sleep(60);
+            return 0;
+        }
+    }
+
+    while (wait(NULL) > 0);
+
+    return 0;
+}
