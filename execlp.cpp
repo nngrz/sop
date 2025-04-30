d4fc4fd97ba964d04bef0d142619f75f560ce740
Ningrui Zhang <ningrui1227@gmail.com>
2025-04-30 15:48:29 +0800

    remove execlp.cpp

--- execlp.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[]) {
-    execlp("/bin/ls", "ls", NULL);
-    printf("Hello world - but this text never appear on any console\n");
-    printf("If so, it means that execlp execution was not successfully ended\n");
-}
